fc7fb8852d85e34f19af501c5eba0339ca2002b0
ishara <ishara9@gmail.com>
2026-02-25 22:15:06 +1300

    basic CRUD done!

--- public/index.php
+++ public/index.php
@@ -3,7 +3,6 @@
 require '../vendor/autoload.php';
 require_once __DIR__ . "/../app/Controllers/PatientController.php";
 
-
 use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
@@ -27,40 +26,3 @@ $app->patch('/api/patients/{id}', [$controller, 'update']);
 $app->delete('/api/patients/{id}', [$controller, 'delete']);
 
 $app->run();
-
-
-// require_once __DIR__ . "/../app/Controllers/PatientController.php";
-
-// $uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-// $method = $_SERVER["REQUEST_METHOD"];
-
-// $controller = new PatientController();
-
-// // switch ($uri) {
-// //     case "/api/patients":
-// //         echo "Hello, World!";
-// //         break;
-// //     default:
-// //         http_response_code(404);
-// //         echo "Not Found";
-// //         break;
-// // }
-
-// $parts = explode("/", trim($uri,'/'));
-
-// // Patient apis
-// if($parts[0] == 'api' && $parts[1] === 'patients'){
-//     if($method === 'GET'){
-//         $controller->getAll();
-//     }
-//     elseif 
-// }
-
-
-// if ($uri === "/api/patients" && $method === "GET") {
-//     $controller->getAll();
-// }
-// elseif($uri === "#^/api/patients/(\d+)$#" && $method === "GET") {
-//     $controller->getById((int) $matches[1]);
-// }
-// elseif()
